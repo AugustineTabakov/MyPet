47875b201ae80f291da7c10bf3eed4189ddd9490
AugustineTabakov <163824029+AugustineTabakov@users.noreply.github.com>
2024-05-06 21:09:46 +0200

    Update MainActivity2.kt

--- app/src/main/java/com/example/mypet/MainActivity2.kt
+++ app/src/main/java/com/example/mypet/MainActivity2.kt
@@ -9,7 +9,8 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity2 : AppCompatActivity() {
-
+// Declaration of Button.
+    
     private lateinit var welcomeButton : Button
 
 
@@ -22,7 +23,9 @@ class MainActivity2 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-
+// Enabling the welcomeButton to take the Introduction screen to the 2nd screen with the attributes displaying the condition
+of the cat.
+        
         val  welcomeButton = findViewById<Button>(R.id.welcomeButton)
 
 
